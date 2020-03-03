2a9eeaa54491b347b1185f95d8c4bdd1d1017f84
maridoh13 <maridoh@gmail.com>
2020-03-03 23:40:00 +0000

    completed all 4_queries

--- 4_queries/coh_avg_assist_dur.sql
+++ 4_queries/coh_avg_assist_dur.sql
@@ -1,1 +1,10 @@
 -- Cohort Average Assistance Duration
+
+SELECT AVG(total_duration) AS avg_total_duration
+FROM (
+  SELECT cohorts.name AS cohort, SUM(assistance_requests.completed_at - assistance_requests.started_at) AS total_duration
+  FROM assistance_requests
+  JOIN students ON student_id = students.id
+  JOIN cohorts ON cohort_id = cohorts.id
+  GROUP BY cohorts.name
+) as total_durations;
---
+++ 4_queries/total_assign_dur.sql
@@ -0,0 +1,6 @@
+-- Total Assignments and duration
+
+SELECT day, COUNT(*) AS number_of_assignments, sum(duration) AS duration
+FROM assignments
+GROUP BY day
+ORDER BY day;
